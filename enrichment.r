c73e83f1d56643da4db67d4f214f2701d4998af5
Fede <federicapersiani28@gmail.com>
2024-10-04 12:28:51 +0200

    fix vaf filter

--- enrichment.r
+++ enrichment.r
@@ -1,8 +1,8 @@
-install.packages("devtools")
+#install.packages("devtools")
 #if(!require(devtools)) install.packages("devtools",repos = "http://cran.rstudio.com/")
 #install.packages("install_github")
-remotes::install_github("wjawaid/enrichR")
-install.packages("jsonlite")
+#remotes::install_github("wjawaid/enrichR")
+#install.packages("jsonlite")
 
 library(jsonlite)
 library(devtools)
